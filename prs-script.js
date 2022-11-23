6f2e6eff55e305076564b40a150a0869336a832b
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-11-23 08:32:50 +0100

    Create getCompueterChoice Function

--- prs-script.js
+++ prs-script.js
@@ -2,6 +2,7 @@
 const prompt = require("prompt-sync")();
 
 //Create a Function (getPlayerChoice) to Grab user inputs as choice of paper, rock or scissors
+//Make user input Case insensitive
 function getPlayerChoice(){
     const playerChoice = (prompt("Please Insert Choice, Paper, Rock or Scissors!")).toUpperCase;
     if (playerChoice == "PAPER" && playerChoice != " "){
@@ -19,7 +20,21 @@ function getPlayerChoice(){
 } 
 //getPlayerChoice();
 
-//Make user input Case insensitive
 //Create a Function (getComputerChoice)to get computer Choice
+function getComputerChoice(){ 
+    const computerChoice = (Math.random());
+    if (computerChoice <= 0.35){
+        console.log(`Computer Choose: Paper`);
+        return "Paper";
+    }else if (computerChoice <= 0.70){
+        console.log(`Computer Choose: Rock`);
+        return "Rock";
+    }else {
+        console.log(`Computer Choose: Scissors`);
+        return "Scissors"
+    }
+}
+//getComputerChoice();
+
 //Create a Function (playRound) to call getPlayerChoice and getComputerChoice to play one round of the game
 //Craete a function (game) to call all three functions and loop the game to play five rounds and update scores.
