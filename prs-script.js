73303a2a5be719bea29309cea4f6242b4893a32d
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-11-23 08:28:51 +0100

    Create getPlayerChoice Function

--- prs-script.js
+++ prs-script.js
@@ -2,7 +2,22 @@
 const prompt = require("prompt-sync")();
 
 //Create a Function (getPlayerChoice) to Grab user inputs as choice of paper, rock or scissors
-
+function getPlayerChoice(){
+    const playerChoice = prompt("Please Insert Choice, Paper, Rock or Scissors!");
+    if (playerChoice == "PAPER" && playerChoice != " "){
+        console.log(`You Choose: Paper`);
+        return "Paper";
+    }else if (playerChoice == "ROCK" && playerChoice !== " "){
+        console.log(`You Choose: Rock`);
+        return "Rock"
+    }else if (playerChoice == "SCISSORS" && playerChoice != " "){
+        console.log(`You Choose: Scissors`);
+        return "Scissors"
+    }else {
+        console.log("Please Insert a Valid Choice of Rock, Paper or Scissors!")
+    }
+} 
+//getPlayerChoice();
 
 //Make user input Case insensitive
 //Create a Function (getComputerChoice)to get computer Choice
