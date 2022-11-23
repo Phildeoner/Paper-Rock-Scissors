526df6089aa4291cf26fc8a89e8893aae97b3d8b
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-11-23 08:26:59 +0100

    Define prompt to link to prompt-sync

--- prs-script.js
+++ prs-script.js
@@ -1,6 +1,9 @@
 //Paper Rock Scissors Game.
+const prompt = require("prompt-sync")();
 
 //Create a Function (getPlayerChoice) to Grab user inputs as choice of paper, rock or scissors
+
+
 //Make user input Case insensitive
 //Create a Function (getComputerChoice)to get computer Choice
 //Create a Function (playRound) to call getPlayerChoice and getComputerChoice to play one round of the game
