e7c3a951a8e5dfb97f5050b6e5fd96b7a99b1c29
PEBY Creative Concepts <87046212+phildeone@users.noreply.github.com>
2022-11-23 08:24:59 +0100

    Update prs-script.js file with pseudocode

---
+++ prs-script.js
@@ -0,0 +1,7 @@
+//Paper Rock Scissors Game.
+
+//Create a Function (getPlayerChoice) to Grab user inputs as choice of paper, rock or scissors
+//Make user input Case insensitive
+//Create a Function (getComputerChoice)to get computer Choice
+//Create a Function (playRound) to call getPlayerChoice and getComputerChoice to play one round of the game
+//Craete a function (game) to call all three functions and loop the game to play five rounds and update scores.
